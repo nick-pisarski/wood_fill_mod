276b627a268a11e8c71fd723a0bca0cfb64b7541
Pisarski, Nick <nick.pisarski@gmail.com>
2025-07-04 10:14:08 -0500

    Add Factorio mod files: created tasks.json for build scripts, added data.lua for wood-fill recipe, and info.json for mod metadata

---
+++ wood-fill/data.lua
@@ -0,0 +1,20 @@
+
+data:extend({
+  {
+    type = "recipe",
+    name = "wood-fill",
+    enabled = true,
+    energy_required = 0.5,
+    ingredients = {
+      {type = "item", name = "wood",amount = 50}
+    },
+    auto_recycle = false,
+    results = {
+      {type = "item", name = "landfill", amount = 1}
+    },
+    category = "crafting",
+    subgroup = "terrain",
+    order = "b[landfill]-a[dirt]",
+    allow_as_intermediate = true,
+  }
+})
